2a6fa3fea9377efc4303c547934d922d90983bf3
jakub.hnilicka <hnilickakuba@gmail.com>
2026-09-08 16:14:43 +0200

    rotate

---
+++ src/myshit/RotatingWindow.java
@@ -0,0 +1,64 @@
+package myshit;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RotatingWindow extends JFrame {
+
+    public List<JPanel> panels = new ArrayList<>();
+    public static List<Color> colors = new ArrayList<>(List.of(Color.red,Color.blue,Color.green,Color.yellow,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE));
+    public static final List<String> layouts = List.of(BorderLayout.NORTH,BorderLayout.WEST,BorderLayout.SOUTH,BorderLayout.EAST,BorderLayout.EAST,BorderLayout.SOUTH,BorderLayout.WEST,BorderLayout.NORTH);
+
+    public RotatingWindow() throws HeadlessException {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLayout(new BorderLayout());
+
+        setSize(500,500);
+        setLocationRelativeTo(null);
+
+        for (int i = 0; i < 4; i++) {
+            JPanel panel = new JPanel();
+            panel.setBackground(colors.get(i));
+            panel.setPreferredSize(new Dimension(100,100));
+            add(panel,layouts.get(i));
+            panels.add(panel);
+        }
+
+        JPanel inner = new JPanel();
+        inner.setLayout(new BorderLayout());
+
+        for (int i = 4; i < 8; i++) {
+            JPanel panel = new JPanel();
+            panel.setBackground(colors.get(i));
+            panel.setPreferredSize(new Dimension(100,100));
+            inner.add(panel,layouts.get(i));
+            panels.add(panel);
+        }
+
+        JButton button = new JButton("Rotate");
+        button.addActionListener(e->{
+//            colors.addFirst(colors.get(colors.size()-1));
+//            colors.removeLast();
+
+            colors.add(colors.getFirst());
+            colors.removeFirst();
+            for (int i = 0; i < panels.size(); i++) {
+                JPanel panel = panels.get(i);
+                panel.setBackground(colors.get(i));
+            }
+        });
+
+
+        inner.add(button);
+
+        add(inner);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new RotatingWindow();
+    }
+}
